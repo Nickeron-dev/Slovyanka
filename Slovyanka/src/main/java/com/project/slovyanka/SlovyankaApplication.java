b1f27b3a7f4e815667e8dae8304bf5d481271c6d
Nickeron-dev <koshkin061969@gmail.com>
2021-11-04 16:49:42 +0200

    Started implementing website.

---
+++ Slovyanka/src/main/java/com/project/slovyanka/SlovyankaApplication.java
@@ -0,0 +1,13 @@
+package com.project.slovyanka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SlovyankaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SlovyankaApplication.class, args);
+    }
+
+}
---
+++ Slovyanka/src/test/java/com/project/slovyanka/SlovyankaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.slovyanka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SlovyankaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
