d4dd1d2f3b4283c588359d892ad9d33319b16018
Nickeron-dev <koshkin061969@gmail.com>
2022-01-16 23:21:04 +0200

    Implemented slider on the main page.

--- Slovyanka/src/main/resources/static/js/main.js
+++ Slovyanka/src/main/resources/static/js/main.js
@@ -26,31 +26,41 @@ function showSlides(n) {
         slides[i].style.display = "none";
         slides[i].style.position = "relative";
         slides[i].style.marginLeft = "0px";
-        slides[i].style.clip = "rect(auto)";
+        slides[i].style.clip = "auto";
     }
     slides[Math.abs(slideIndex)].style.display = "block";
-    // slides[Math.abs(slideIndex)].style.float = "left";
     slides[Math.abs(slideIndex)].style.position = "absolute";
     slides[Math.abs(slideIndex)].style.marginLeft = "0px";
     slides[Math.abs(slideIndex)].style.clip = "rect(0px, 460px, 460px, 0px)";
-    let next = Math.abs(slideIndex + 1) >= slides.length ? 0 : Math.abs(slideIndex + 1);
+    let next = Math.abs(slideIndex);
+    if (next >= slides.length - 1){
+        next = 0;
+    }
+    else {
+        next++;
+    }
     slides[next].style.display = "block";
     slides[next].style.position = "absolute";
     slides[next].style.marginLeft = "460px";
 
-    let next2 = Math.abs(slideIndex + 2) >= slides.length ? 1 : Math.abs(slideIndex + 2);
-    slides[next2].style.display = "block";
-    slides[next2].style.position = "absolute";
-    slides[next2].style.marginLeft = "920px";
-    let next3 = Math.abs(slideIndex + 3) >= slides.length ? 2 : Math.abs(slideIndex + 3);
-    slides[next3].style.display = "block";
-    slides[next3].style.position = "absolute";
-    slides[next3].style.marginLeft = "1380px";
-    slides[next3].style.clip = "rect(0px, 58px, 400px, -345px)";
-
-    if (n >= slides.length - 1) {
-        slideIndex = -1;
+    if (next >= slides.length - 1){
+        next = 0;
     }
-    slides[(slideIndex + 1) === slides.length ? 0 : slideIndex + 1].style.display = "block";
-    slides[(slideIndex + 1) === slides.length ? 0 : slideIndex + 1].style.float = "right";
+    else {
+        next += 1;
+    }
+    slides[next].style.display = "block";
+    slides[next].style.position = "absolute";
+    slides[next].style.marginLeft = "920px";
+
+    if (next >= slides.length - 1){
+        next = 0;
+    }
+    else {
+        next++;
+    }
+    slides[next].style.display = "block";
+    slides[next].style.position = "absolute";
+    slides[next].style.marginLeft = "1380px";
+    slides[next].style.clip = "rect(0px, 58px, 400px, -345px)";
 }
